be1b84c52250f3b8f7b439984b878f47a919a174
hhoover <hart.hoover@gmail.com>
2020-02-09 19:54:15 -0600

    compress hugo site into a tar.gz

--- intro-to-containers/setup.sh
+++ intro-to-containers/setup.sh
@@ -3,4 +3,6 @@ cd ~
 service docker restart
 docker pull alpine
 docker pull nginx:stable-alpine
+tar -zxf /home/scrapbook/hugosite.tar.gz
+rm -f /home/scrapbook/hugosite.tar.gz
 echo "Welcome to Into to Containers!"
