c7b05a5ebb484a67a11ed8620f8ce1776db25cd7
hhoover <hart.hoover@gmail.com>
2020-02-12 11:40:53 -0600

    move all assets into setup script

--- intro-to-containers/setup.sh
+++ intro-to-containers/setup.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
+mkdir ~/kubernetes-yaml
+cd ~/kubernetes-yaml
+curl -LO https://raw.githubusercontent.com/hhoover/intro/master/intro-to-containers/assets/deployment.yaml
+curl -LO https://raw.githubusercontent.com/hhoover/intro/master/intro-to-containers/assets/service.yaml
 cd ~
 curl -LO https://raw.githubusercontent.com/hhoover/intro/master/intro-to-containers/assets/hugosite.tar.gz
 tar -zxf ~/hugosite.tar.gz
 rm -f ~/hugosite.tar.gz
-service docker restart
 docker pull alpine
 docker pull nginx:stable-alpine
 docker pull registry:2
